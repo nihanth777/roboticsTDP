21e08e09c97b1105de4aa48a30a9c6a3b52b363c
Cecilia-Yuge-Wang <2809017W@student.gla.ac.uk>
2023-02-27 14:34:34 +0000

    change it as a plot function to show the moving direction

--- new code/functions/nextPosition.m
+++ new code/functions/nextPosition.m
@@ -1,8 +1,9 @@
-function [X,Y] = nextPosition(x,y,phi,l)
-%NEXTPOSITION is a function used to calculate the next position wrt
+function nextPosition(x,y,phi,l)
+%NEXTPOSITION is a function used to plot the moving direction wrt
 %distance and heading angle.
 %   Detailed explanation goes here
 X=x+l*cos(phi);
 Y=y+l*sin(phi);
+plot([x X],[y Y],"Color","black","LineStyle","-")
 end
 
