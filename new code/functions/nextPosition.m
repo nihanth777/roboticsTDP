465caa7354566e9253349af33bb8f6826bf47b47
nihanth777 <nihanthmanyam@gmail.com>
2023-03-02 16:10:28 +0000

    Delete new code directory

--- new code/functions/nextPosition.m
+++
@@ -1,9 +0,0 @@
-function nextPosition(x,y,phi,l)
-%NEXTPOSITION is a function used to plot the moving direction wrt
-%distance and heading angle.
-%   Detailed explanation goes here
-X=x+l*cos(phi);
-Y=y+l*sin(phi);
-plot([x X],[y Y],"Color","black","LineStyle","-")
-end
-
--- new code/functions/phicalculate.m
+++
@@ -1,5 +0,0 @@
-function phi = phicalculate(X1,Y1,X2,Y2)
-       
-        phi = atan((Y2-Y1)/(X2-X1));
-        
-end
