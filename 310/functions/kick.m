daab9981944607c8b145508361d9bb2c7648172c
nihanth777 <nihanthmanyam@gmail.com>
2023-04-09 05:32:55 +0100

    new changes.. behaviour skeleton code and tasks in robotbehaviour page bottom comments

--- 310/functions/kick.m
+++ 310/functions/kick.m
@@ -22,8 +22,8 @@ else
             rphi_new = r_phi-omega*sample_time+noise;
     
     end
-     players{2}(indexOfPlayers,2) = rphi_new;
-     ball(1,1)=players{4}(indexOfPlayers,1);
-     ball(1,2)=players{4}(indexOfPlayers,2);
+    players{2}(indexOfPlayers,2) = rphi_new;
+    ball(1,1)=players{4}(indexOfPlayers,1);
+    ball(1,2)=players{4}(indexOfPlayers,2);
 end
 end
